e0c216928f6f244842fd4740024f5e4b567bb4af
Viseth Sen <viseth89@gmail.com>
2017-01-07 22:31:11 -0600

    6 charts with stats working
    finding a better way to display whole such as 50 out of 54 with the
    chart

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,8 +1,10 @@
 import csv
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_wtf import Form
 from wtforms import FileField
+from bullstats import *
+
 app = Flask(__name__)
 
 class UploadForm(Form):
@@ -15,15 +17,18 @@ def index():
     body = 'this is the body text'
     home_score = []
     away_score = []
+    rebow = len(trebounds_won)
 
-    with open('/var/www/FlaskApp/FlaskApp/static/bulls1996.csv') as filename:
+
+    with open('bulls1996.csv') as filename:
         chart_data = csv.DictReader(filename)
         for row in chart_data:
             home_score.append(int(row['Tm']))
             away_score.append(int(row['Opp']))
 
 
-    return render_template('index.html', title=title, body=body, home_score=home_score, away_score=away_score)
+
+    return render_template('index.html', title=title, body=body, home_score=rebow, away_score=away_score)
 
 
 
@@ -45,24 +50,40 @@ def upload(request):
 
 
 
-
-
-
 @app.route("/bulls")
 def bulls():
     title = 'this is the title'
     body = 'this is the body text'
     home_score = []
     away_score = []
+    reboundsa = len(trebounds_won)
+    reboundsb = len(trebounds_loss)
+    oreboundsa = len(orebounds_won)
+    oreboundsb = len(orebounds_loss)
+    assista = len(assist_won)
+    assistb = len(assist_loss)
+    edgea = len(astrebw)
+    edgeb = len(astreb)
+    turnovera = len(turnover_w)
+    turnoverb = len(turnover_l)
+    steala = len(steal_won)
+    stealb = len(steal_loss)
+    blocksa = len(blocks_won)
+    blocksb = len(blocks_loss)
 
-    with open('/var/www/FlaskApp/FlaskApp/static/bulls1996.csv') as filename:
+    print (len(astreb))
+    print (home_score)
+
+    with open('bulls1996.csv') as filename:
         chart_data = csv.DictReader(filename)
         for row in chart_data:
             home_score.append(int(row['Tm']))
             away_score.append(int(row['Opp']))
 
 
-    return render_template('charts.html', title=title, body=body, home_score=home_score, away_score=away_score)
+    return render_template('charts.html', title=title, body=body, home_score=home_score, away_score=away_score, edgea=edgea, edgeb=edgeb, steala=steala, stealb=stealb, turnovera=turnovera, turnoverb=turnoverb, blocksa=blocksa,blocksb=blocksb, oreboundsa=oreboundsa,oreboundsb=oreboundsb,
+    reboundsa=reboundsa,reboundsb=reboundsb,
+    assista = assista, assistb=assistb)
 
 
 
