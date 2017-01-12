8e54f53b8efaa9875bea21b86cd09af60dd0628d
Viseth Sen <viseth89@gmail.com>
2017-01-12 16:03:26 -0600

    before presentation
    uploading before presenting

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -6,7 +6,7 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_wtf import FlaskForm
 from wtforms import FileField
 from werkzeug.utils import secure_filename
-from Read_Game import Read_Game
+from ReadGame import ReadGame
 
 app = Flask(__name__)
 
@@ -14,8 +14,6 @@ photos = UploadSet('photos', IMAGES)
 
 app.config['UPLOADED_PHOTOS_DEST']= 'static/img'
 configure_uploads(app,photos)
-
-file_dir = ''
 
 class UploadForm(FlaskForm):
     file = FileField()
@@ -41,9 +39,8 @@ def index():
 @app.route("/bulls")
 def bulls():
 
-    b = Read_Game('/var/www/FlaskApp/FlaskApp/static/bulls1996.csv')
+    b = ReadGame('/var/www/FlaskApp/FlaskApp/static/bulls1996.csv')
     title = 'The Champion 1996 Chicago Bulls'
-    body = 'The first of second 3peat'
     reboundsa = len(b.trebounds_won)
     reboundsb = len(b.trebounds_loss)
     oreboundsa = len(b.orebounds_won)
@@ -58,21 +55,126 @@ def bulls():
     stealb = len(b.steal_loss)
     blocksa = len(b.blocks_won)
     blocksb = len(b.blocks_loss)
-
     #Will be removing extra lines of code soon, left only to display previous way of doing compared to newer, evolved ways below
 
     return render_template('charts.html',
     title=title,
-    body=body,
     edgea=edgea, edgeb=edgeb,
     steala=steala, stealb=stealb,
     turnovera=turnovera, turnoverb=turnoverb,
     blocksa=blocksa,blocksb=blocksb,
     oreboundsa=oreboundsa,oreboundsb=oreboundsb,
     reboundsa=reboundsa,reboundsb=reboundsb,
-    assista = assista, assistb=assistb
+    assista = assista, assistb=assistb,
+    wins = len(b.wins),
+    loss = len(b.loss)
     )
 
+@app.route("/grizzlies")
+def grizzlies():
+
+    g = ReadGame('/var/www/FlaskApp/FlaskApp/static/grizzlies1996.csv')
+    title = 'Looking at the Worse 1996 NBA Team'
+    body = 'Mean But True'
+
+    return render_template('gcharts.html',
+    title=title,
+    body=body,
+    edgea=len(g.astrebw), edgeb=len(g.astreb),
+    steala=len(g.steal_won), stealb=len(g.steal_loss),
+    turnovera = len(g.turnover_w), turnoverb = len(g.turnover_l),
+    blocksa = len(g.blocks_won),blocksb = len(g.blocks_loss),
+    oreboundsa=len(g.orebounds_won),oreboundsb=len(g.orebounds_loss),
+    reboundsa=len(g.trebounds_won),reboundsb=len(g.trebounds_loss),
+    assista = len(g.assist_won), assistb=len(g.assist_loss),
+    wins = len(g.wins),
+    loss = len(g.loss)
+    )
+
+@app.route("/warriors")
+def warriors():
+
+    w = ReadGame('/var/www/FlaskApp/FlaskApp/static/warriors2016.csv')
+    title = 'Looking at the Best 2016 NBA Team'
+    body = 'Record Breaking'
+
+    return render_template('wcharts.html',
+    title=title,
+    body=body,
+    edgea=len(w.astrebw), edgeb=len(w.astreb),
+    steala=len(w.steal_won), stealb=len(w.steal_loss),
+    turnovera = len(w.turnover_w), turnoverb = len(w.turnover_l),
+    blocksa = len(w.blocks_won),blocksb = len(w.blocks_loss),
+    oreboundsa=len(w.orebounds_won),oreboundsb=len(w.orebounds_loss),
+    reboundsa=len(w.trebounds_won),reboundsb=len(w.trebounds_loss),
+    assista = len(w.assist_won), assistb=len(w.assist_loss),
+    wins = len(w.wins),
+    loss = len(w.loss)
+    )
+
+@app.route("/phil")
+def phil():
+
+    p = ReadGame('/var/www/FlaskApp/FlaskApp/static/76ers.csv')
+    title = 'Looking at the Worse 2016 NBA Team'
+    body = 'Tear Dropping 10 - 72 :( '
+
+    return render_template('pcharts.html',
+    title=title,
+    body=body,
+    edgea=len(p.astrebw), edgeb=len(p.astreb),
+    steala=len(p.steal_won), stealb=len(p.steal_loss),
+    turnovera = len(p.turnover_w), turnoverb = len(p.turnover_l),
+    blocksa = len(p.blocks_won),blocksb = len(p.blocks_loss),
+    oreboundsa=len(p.orebounds_won),oreboundsb=len(p.orebounds_loss),
+    reboundsa=len(p.trebounds_won),reboundsb=len(p.trebounds_loss),
+    assista = len(p.assist_won), assistb=len(p.assist_loss),
+    wins = len(p.wins),
+    loss = len(p.loss)
+    )
+
+
+@app.route('/guide')
+def guide():
+
+
+    title = 'How to make your own site'
+    body = 'I will show you how'
+
+    return render_template('siteguide.html')
+
+@app.route('/google')
+def google():
+
+    p = ReadGame('/var/www/FlaskApp/FlaskApp/static/76ers.csv')
+    return render_template('google.html',assista = len(p.assist_won), assistb=len(p.assist_loss))
+
+
+
+#Upload #1 Priority is to get upload file working
+#Barkley is our test for the player
+#Finals 2016 will be our test for the 7 game finals
+
+"""
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+
+    if form.validate_on_submit():
+        filename = secure_filename(form.file.data.filename)
+        form.file.data.save('uploads/' + filename)
+        return redirect('index.html')
+
+    return render_template('upload.html', form=form)
+
+@app.route("/barkley")
+def barkley():
+    title = 'Charles Barkley'
+    body = 'Round Mound '
+
+    return render_template('player.html',
+
+    )
 
 @app.route("/finals2016")
 def finals2016():
@@ -90,86 +192,7 @@ def finals2016():
     reboundsa=len(b.trebounds_won),reboundsb=len(b.trebounds_loss),
     assista = len(b.assist_won), assistb=len(b.assist_loss)
     )
-
-@app.route("/grizzlies")
-def grizzlies():
-
-    g = Read_Game('/var/www/FlaskApp/FlaskApp/static/grizzlies1996.csv')
-    title = 'Looking at the Worse 1996 NBA Team'
-    body = 'Mean But True'
-
-    return render_template('gcharts.html',
-    title=title,
-    body=body,
-    edgea=len(g.astrebw), edgeb=len(g.astreb),
-    steala=len(g.steal_won), stealb=len(g.steal_loss),
-    turnovera = len(g.turnover_w), turnoverb = len(g.turnover_l),
-    blocksa = len(g.blocks_won),blocksb = len(g.blocks_loss),
-    oreboundsa=len(g.orebounds_won),oreboundsb=len(g.orebounds_loss),
-    reboundsa=len(g.trebounds_won),reboundsb=len(g.trebounds_loss),
-    assista = len(g.assist_won), assistb=len(g.assist_loss)
-    )
-
-@app.route("/warriors")
-def warriors():
-
-    w = Read_Game('/var/www/FlaskApp/FlaskApp/static/warriors2016.csv')
-    title = 'Looking at the Best 2016 NBA Team'
-    body = 'Record Breaking'
-
-    return render_template('wcharts.html',
-    title=title,
-    body=body,
-    edgea=len(w.astrebw), edgeb=len(w.astreb),
-    steala=len(w.steal_won), stealb=len(w.steal_loss),
-    turnovera = len(w.turnover_w), turnoverb = len(w.turnover_l),
-    blocksa = len(w.blocks_won),blocksb = len(w.blocks_loss),
-    oreboundsa=len(w.orebounds_won),oreboundsb=len(w.orebounds_loss),
-    reboundsa=len(w.trebounds_won),reboundsb=len(w.trebounds_loss),
-    assista = len(w.assist_won), assistb=len(w.assist_loss)
-    )
-
-@app.route("/phil")
-def phil():
-
-    p = Read_Game('/var/www/FlaskApp/FlaskApp/static/76ers.csv')
-    title = 'Looking at the Worse 2016 NBA Team'
-    body = 'Tear Dropping 10 - 72 :( '
-
-    return render_template('pcharts.html',
-    title=title,
-    body=body,
-    edgea=len(p.astrebw), edgeb=len(p.astreb),
-    steala=len(p.steal_won), stealb=len(p.steal_loss),
-    turnovera = len(p.turnover_w), turnoverb = len(p.turnover_l),
-    blocksa = len(p.blocks_won),blocksb = len(p.blocks_loss),
-    oreboundsa=len(p.orebounds_won),oreboundsb=len(p.orebounds_loss),
-    reboundsa=len(p.trebounds_won),reboundsb=len(p.trebounds_loss),
-    assista = len(p.assist_won), assistb=len(p.assist_loss)
-    )
-
-
-@app.route('/guide')
-def guide():
-
-    title = 'How to make your own site'
-    body = 'I will show you how'
-
-    return render_template('siteguide.html')
-
-
-
-#Barkley is our test
-
-@app.route("/barkley")
-def barkley():
-    title = 'Charles Barkley'
-    body = 'Round Mound '
-
-    return render_template('player.html',
-
-    )
-
+"""
 if __name__ == "__main__":
     app.debug=True
     app.run()
